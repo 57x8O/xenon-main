d65102b7814f830ceb1506ef0081f500186a5023
Merlintor <merlinfuchs2001@gmail.com>
2020-07-31 22:54:08 +0200

    debug 2

--- run.py
+++ run.py
@@ -2,11 +2,13 @@ from bot import Xenon
 from os import environ as env
 
 
+raise ValueError(env.get("TOKEN"))
+
+
 bot = Xenon(
     prefix=env.get("PREFIX") or "#!",
     mongo_url=env.get("MONGO_URL") or "mongodb://localhost",
     rabbit_url=env.get("RABBIT_URL") or "amqp://guest:guest@localhost/",
     redis_url=env.get("REDIS_URL") or "redis://localhost/"
 )
-print(env.get("TOKEN"))
 bot.run(token=env.get("TOKEN"), shared_queue="main")
