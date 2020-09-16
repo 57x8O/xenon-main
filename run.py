7edbb2282201c1907e5e3e157897ffbcf5bc365a
Merlintor <merlinfuchs2001@gmail.com>
2020-09-16 21:20:24 +0200

    Make redis db index configurable

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ bot = Xenon(
     prefix=env.get("PREFIX") or "#!",
     mongo_url=env.get("MONGO_URL") or "mongodb://localhost",
     rabbit_url=env.get("RABBIT_URL") or "amqp://guest:guest@localhost/",
-    redis_url=env.get("REDIS_URL") or "redis://localhost/"
+    redis_url=env.get("REDIS_URL") or "redis://localhost/",
+    redis_db=int(env.get("REDIS_DB") or "0")
 )
 bot.run(token=env.get("TOKEN"), shared_queue="main")
