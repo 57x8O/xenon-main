e5dcf774d2bf580e6bc186e8774ac96a416e0282
Merlintor <merlinfuchs2001@gmail.com>
2020-10-05 17:57:14 +0200

    Add check for server specific permission levels

--- checks.py
+++ checks.py
@@ -1,8 +1,8 @@
 import xenon_worker as wkr
-from enum import Enum
+from enum import IntEnum
 
 
-class StaffLevel(Enum):
+class StaffLevel(IntEnum):
     NONE = -1
     MOD = 0
     ADMIN = 1
@@ -25,6 +25,45 @@ def is_staff(level=StaffLevel.MOD):
                 raise NotStaff(current=StaffLevel(staff["level"]), required=level)
 
             return True
+
+        return wkr.Check(check, callback)
+
+    return predicate
+
+
+class PermissionLevels(IntEnum):
+    ADMIN_ONY = 0
+    DESTRUCTIVE_OWNER = 1
+    OWNER_ONLY = 2
+
+
+def has_permissions_level(destructive=False):
+    def predicate(callback):
+        async def check(ctx, *args, **kwargs):
+            settings = await ctx.bot.db.guilds.find_one({"_id": ctx.guild_id})
+            if settings is None or "permissions_level" not in settings:
+                required = PermissionLevels.DESTRUCTIVE_OWNER
+
+            else:
+                required = PermissionLevels(settings["permissions_level"])
+
+            if required == PermissionLevels.OWNER_ONLY:
+                try:
+                    return await wkr.is_owner(callback)
+                except wkr.NotOwner:
+                    raise
+
+            elif required == PermissionLevels.DESTRUCTIVE_OWNER and destructive:
+                try:
+                    return await wkr.is_owner(callback)
+                except wkr.NotOwner:
+                    raise
+
+            else:
+                try:
+                    return await wkr.has_permissions(administrator=True)(callback)
+                except wkr.MissingPermissions:
+                    raise
 
         return wkr.Check(check, callback)
 
